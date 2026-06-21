f0cd3b49fd6f28ecf3fc4c18f635a5652daedf3c
yadnp2 <yadnp2@gmail.com>
2026-06-21 14:19:13 -0400

    Remove extra HTTP headers from page requests
    Removed setting of extra HTTP headers for page requests.

--- index.js
+++ index.js
@@ -158,18 +158,6 @@ async function takeScreenshotBrowserless(url) {
       failedResources.push(request.url() + ' - ' + (failure ? failure.errorText : 'unknown'));
     });
 
-    await page.setExtraHTTPHeaders({
-      'Accept-Language': 'en-US,en;q=0.9',
-      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
-      'Accept-Encoding': 'gzip, deflate, br',
-      'Connection': 'keep-alive',
-      'Upgrade-Insecure-Requests': '1',
-      'Sec-Fetch-Dest': 'document',
-      'Sec-Fetch-Mode': 'navigate',
-      'Sec-Fetch-Site': 'none',
-      'Sec-Fetch-User': '?1',
-    });
-
     await page.setCookie({
       name: 'SRCHHPGUSR',
       value: 'ADLT=OFF',
